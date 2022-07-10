102458e09fbc7d94a1bdd2736dd8348ac6896580
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2022-07-10 10:37:16 +0200

    Add initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# yapermutation
+# YA-PERMUTATION
 yet another C/C++ permutation algorithm implementation. Header only C/C++ permutation algorithm
+
+## Usage Notes
+Keep note that the complexity of this algorithm increases exponentially: The more choices you provide, the more time it will take.
+
+This library is _not_ the same as [`std::next_permutation`](https://en.cppreference.com/w/cpp/algorithm/next_permutation)
+
+## Help Wanted
+If you have a suggestion on how to decrease the complexity, please submit an issue or pull-request
+
+## Example Usage
+```c++
+#include <permutation> // include the library
+
+// define a function that combines the permutations into some type
+// in this case, we combine a list of ints to a std::string
+auto combiner_function(const ya::combiner_iterator_list_t<int>& iterator_list) -> std::string {
+    std::stringstream ss{};
+    for(auto& it : iterator_list)
+        ss << *it;
+    return ss.str();
+}
+
+int main(int argc, char** argv) {
+    // construct the different choices
+    std::vector<std::vector<int>> choices = {{0,1},{0,1},{1,2,3}};
+    // call the library
+    auto permutations = ya::generate_permutations(choices, combiner_function);
+    // go through the generated permutations
+    for(auto& perm : permutations)
+        std::cout << perm << std::endl;
+    return 0;
+}
+// RESULT:
+// 001
+// 002
+// 003
+// 011
+// 012
+// 013
+// 101
+// 102
+// 103
+// 111
+// 112
+// 113
+```
+
---
+++ include/implementation/permutation.h
@@ -0,0 +1,79 @@
+/* MIT License
+
+Copyright (c) 2022 Asger Gitz-Johansen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+#ifndef YAPERMUTATION_PERMUTATION_H
+#define YAPERMUTATION_PERMUTATION_H
+#include <numeric>
+#include <vector>
+
+namespace ya {
+    struct _n_digit_num_t {
+        std::vector<size_t> max_digits;
+        std::vector<size_t> number;
+        _n_digit_num_t(std::vector<size_t>&& m, std::vector<size_t>&& n) : max_digits{std::move(m)}, number{std::move(n)} {}
+        void operator++() {
+            for(size_t i = number.size()-1; i >= 0; i--) {
+                if(number[i] < max_digits[i]) {
+                    number[i]++;
+                    break;
+                }
+                number[i] = 0;
+            }
+        }
+    };
+
+    template<typename T>
+    using combiner_iterator_t = typename std::vector<T>::const_iterator;
+    template<typename T>
+    using combiner_iterator_list_t = std::vector<combiner_iterator_t<T>>;
+    template<typename R, typename T>
+    using combiner_funct_t = R(*)(const combiner_iterator_list_t<T>&);
+
+    template<typename T, typename R>
+    auto generate_permutations(const std::vector<std::vector<T>>& input, combiner_funct_t<R,T> combiner) -> std::vector<R> {
+        std::vector<size_t> m{}; m.reserve(input.size());
+        std::vector<size_t> n{}; n.reserve(input.size());
+        for(auto& v : input) {
+            n.push_back(0);
+            if(v.empty()) {
+                m.push_back(0);
+                continue;
+            }
+            m.push_back(v.size()-1);
+        }
+        auto plus_one_mult = [](const size_t& a, const size_t& b){ return a * (b+1); };
+        auto max_num = std::accumulate(m.begin(), m.end(), 1, plus_one_mult);
+        _n_digit_num_t tt{std::move(m),std::move(n)};
+        std::vector<R> result{}; result.reserve(max_num);
+        std::vector<typename std::vector<T>::const_iterator> c{}; c.reserve(tt.number.size());
+        for(int i = 0; i < max_num; i++) {
+            c.clear();
+            for(size_t j = 0; j < tt.number.size(); j++)
+                c.push_back(input[j].begin() + tt.number[j]);
+            result.push_back(combiner(c));
+            ++tt;
+        }
+        return result;
+    }
+}
+
+#endif //YAPERMUTATION_PERMUTATION_H
